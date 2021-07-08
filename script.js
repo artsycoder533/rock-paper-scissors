38f2cb0f3f34b9674ed8056f9d37203922cbd595
Natasha <njohnson533@gmail.com>
2021-07-07 20:38:55 -0400

    Added game function

--- script.js
+++ script.js
@@ -21,19 +21,45 @@ function playRound(player, computer) {
     //check for player win
     else if((player == "rock" && computer == "scissors") || (player == "paper" && computer == "rock") || (player == "scissors" && computer == "paper")){
         //return outcome
+        playerwins++;
         return `Winner! ${player} beats ${computer}!`;
     }
         
     //check for player loss
     else if((computer == "rock" && player == "scissors") || (computer == "paper" && player == "rock") || (computer == "scissors" && player == "paper")){
         //return outcome
+        computerWins++;
         return `You lose! ${computer} beats ${player}!`;
     }
 }
 
+//game function
+//takes in one parameter computers selection
+function game(computer) {
+    //initialize player score, computer score, and counter to 0
+    let playerWins = 0;
+    let computerWins = 0;
+    let rounds = 5;
+    //calls playRound 5 times
+    for(let i = 0; i < rounds; i++){
+        //get player selection
+        let playerMove = prompt("Enter rock, paper, or scissors");
+        playRound(playerMove,computerPlay());
+    }
+    //whoever wins more games out of 5 wins
+    //returns overall winner
+    if (playerWins == computerWins){
+        return "Its a draw. There is no winner :(";
+    }
+    else if(playerWins > computerWins){
+        return "Player beat the computer!";
+    }
+    else {
+        return "Computer beat the player!";
+    }
+    
+}
+
+game();
 
 
-console.log(playRound("rock", computerPlay()));
-console.log(playRound("paper", computerPlay()));
-console.log(playRound("scissors", computerPlay()));
-//computerPlay();
