374b5ae7f8db2aa4d1c79cf0bb295395ea9996c4
Natasha <njohnson533@gmail.com>
2021-07-10 22:30:07 -0400

    Updated UI, changed logic to account for UI functionality in script.js, updated style.css

--- script.js
+++ script.js
@@ -1,52 +1,78 @@
 //variables
-const startGame = document.querySelector('.startGame');
+const newGame = document.querySelector('.newGame');
 const rock = document.querySelector('.rock');
 const rockIcon = document.querySelector('.playerMove .fa-hand-rock');
 const paper = document.querySelector('.paper');
 const paperIcon = document.querySelector('.playerMove .fa-hand-paper');
 const scissors = document.querySelector('.scissors');
 const scissorsIcon = document.querySelector('.playerMove .fa-hand-scissors');
+const playerButtons = document.querySelector('.playerButtons');
 //comuter variables
 const rockIconComp = document.querySelector('.computerMove .fa-hand-rock');
 const paperIconComp = document.querySelector('.computerMove .fa-hand-paper');
 const scissorsIconComp = document.querySelector('.computerMove .fa-hand-scissors');
-const player = document.getElementsByClassName('.playerScore');
 //player score
-const playScoreDisplay = document.getElementById('#playerScore');
+const playerScore = document.querySelector('.playerScore');
+const playScoreDisplay = document.querySelector('#playerScore');
 //computer score
+const computerScore = document.querySelector('.computerScore');
+const computerScoreDisplay = document.querySelector('#computerScore');
+//game updates
+const status = document.querySelector('.status');
+//global variables
+let playerWins = 0;
+let computerWins = 0;
+const wins = 5;
+let name = "";
 
 //event listeners
-startGame.addEventListener('click', () => {
-    let name = prompt("Whats your name?");
+newGame.addEventListener('click', () => {
+    playScoreDisplay.textContent = 0;
+    computerScoreDisplay.textContent = 0;
+    name = prompt("Whats your name?");
+    //wins = parseInt(prompt("What would you like to play to? [choose an integer between 1 & 10]"));
     const newh2 = document.querySelector('.score-p');
-    const x = document.querySelector('.playerScore');
     newh2.textContent = `${name}`;
-    x.appendChild(newh2);
+    const span = document.querySelector('span');
+    span.textContent = `${wins}`;
+    //starts game
+    //make new game button invisible
+    //document.querySelector(".newGame").disabled = true; 
+    playerButtons.classList.remove('hide');
+    newGame.classList.add('hide');
+   // game(name, rounds);
+
 });
-rock.addEventListener('click', () => {
+
+rock.addEventListener('click', () => { 
     //display rock icon
     rockIcon.classList.remove('displayIcon');
     //turn off other icons
     paperIcon.classList.add('displayIcon');
     scissorsIcon.classList.add('displayIcon');
     //call playRound function with corresponding player selection as first parameter
-    playRound(rock, computerPlay());
+    let w = playRound("rock", computerPlay());
+    //alert(roundWinner.value);
+    checkWinner(w);
 });
 paper.addEventListener('click', () => {
     paperIcon.classList.remove('displayIcon');
     //turn off other icons
     rockIcon.classList.add('displayIcon');
     scissorsIcon.classList.add('displayIcon');
-    playRound(paper, computerPlay());
+    let y = playRound("paper", computerPlay());
+    checkWinner(y);
 });
 scissors.addEventListener('click', () => {
     scissorsIcon.classList.remove('displayIcon');
     //turn off other icons
     rockIcon.classList.add('displayIcon');
     paperIcon.classList.add('displayIcon');
-    playRound(scissors, computerPlay());
+    let z = playRound("scissors", computerPlay());
+    checkWinner(z);
 });
 
+//funtions
 function showComputerPlay (move) {
     //rock
     if (move === "rock"){
@@ -77,7 +103,6 @@ function showComputerPlay (move) {
 //computer play
 //randomly return either "rock", "paper" or "scissors"
 function computerPlay() {
-    //create an array with three strings, rock, paper, and scissors
     const compPlay = ["rock", "paper", "scissors"];
     //use a random number generator to produce a number between 0 and 2
     let spot = Math.floor(Math.random() * 3);
@@ -88,7 +113,6 @@ function computerPlay() {
 }
 
 //play round
-//take in two parameters, players selection and computers selection
 function playRound(player, computer) {
     //keeps track of who wins the round
     let roundWinner = "";
@@ -98,59 +122,97 @@ function playRound(player, computer) {
         //return outcome
         roundWinner = "none";
         //return "Draw! No Winner."
+        status.textContent = "Draw! No Winner.";
     }
     //check for player win
     else if((player == "rock" && computer == "scissors") || (player == "paper" && computer == "rock") || (player == "scissors" && computer == "paper")){
         //return outcome
         roundWinner = "player";
-        playerScoreDisplay.textContent = "1";
-        player.appendChild(playerScoreDisplay);
-        //return `Winner! ${player} beats ${computer}!`;
+        //playerScoreDisplay.textContent = "1";
+        status.textContent = `Winner! ${player} beats ${computer}!`;
+        
     }
         
     //check for player loss
     else if((computer == "rock" && player == "scissors") || (computer == "paper" && player == "rock") || (computer == "scissors" && player == "paper")){
         //return outcome
         roundWinner = "computer";
-        //return `You lose! ${computer} beats ${player}!`;
+        //computerScoreDisplay.textContent = "1";
+        status.textContent = `You lose! ${computer} beats ${player}!`;
     }
     return roundWinner;
 }
 
-//game function
-//takes in one parameter computers selection
-function game() {
-    //initialize player score, computer score, and counter to 0
-    let playerWins = 0;
-    let computerWins = 0;
-    let rounds = 5;
-    let winnerOfRound = "";
-    //calls playRound 5 times
-    for(let i = 0; i < rounds; i++){
-        //get player selection
-        //let playerMove = prompt("Enter rock, paper, or scissors");
-        //plays a round and keeps track of who won the round;
-        winnerOfRound = playRound(playerMove,computerPlay());
-        //keep track of wins
-        if (winnerOfRound == "player"){
-            playerWins++;
-        }
-        else if(winnerOfRound == "computer"){
-            computerWins++;
+function checkWinner(winner){
+    //continue while neither players wins equals wins variable
+    if (winner === "player"){
+        playerWins++;
+        playScoreDisplay.textContent = `${playerWins}`;
+        //check for game won
+        if(wins === playerWins){
+            gameWon(winner);
         }
     }
-    //whoever wins more games out of 5 wins
-    //returns overall winner
-    if (playerWins == computerWins){
-        return "Its a draw. There is no winner :(";
-    }
-    else if(playerWins > computerWins){
-        return "Player beat the computer!";
-    }
-    else {
-        return "Computer beat the player!";
+    else if (winner === "computer"){
+        computerWins++;
+        computerScoreDisplay.textContent = `${computerWins}`;
+        if(wins === computerWins){
+            gameWon(winner);
+        }
     }
 }
+
+function gameWon(gameWinner){
+      if (gameWinner === "player"){
+        status.textContent = `${name} is the winner!!!!!`;
+        status.classList.add('winner');
+        newGame.classList.remove('hide');
+        
+    }
+
+    else if(gameWinner === "computer"){
+        status.textContent = "GAME OVER :(";
+        status.classList.add('winner');
+        newGame.classList.remove('hide');
+    }
+    document.querySelector('.newGame').disabled = false; 
+    playerButtons.classList.add('hide');
+}
+
+// //game function
+// //takes in one parameter computers selection
+// function game(playerName, numRounds) {
+//     //initialize player score, computer score, and counter to 0
+//     let playerWins = 0;
+//     let computerWins = 0;
+//     //let rounds = 5;
+//     let winnerOfRound = "";
+//     //calls playRound 5 times
+    
+//         //get player selection
+//         //let playerMove = prompt("Enter rock, paper, or scissors");
+//         //plays a round and keeps track of who won the round;
+//         winnerOfRound = playRound(playerMove,computerPlay());
+//         //keep track of wins
+//         if (winnerOfRound == "player"){
+//             playerWins++;
+//         }
+//         else if(winnerOfRound == "computer"){
+//             computerWins++;
+//         }
+    
+//     //whoever wins more games out of 5 wins
+//     //returns overall winner
+//     if (playerWins == computerWins){
+//         return "Its a draw. There is no winner :(";
+//     }
+//     else if(playerWins > computerWins){
+//         return "Player beat the computer!";
+//     }
+//     else {
+//         return "Computer beat the player!";
+//     }
+// }
 
 //game();
 
